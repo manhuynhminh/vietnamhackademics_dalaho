10ccb31ce3565db90ad3aa20d6aca21dec722a13
2tran <manhhai_thk7@yahoo.com.vn>
2015-08-01 17:30:42 +0700

    Is Working

--- MainPage.php
+++ MainPage.php
@@ -3,7 +3,7 @@
 
 
                 <h2>Theo dõi kết quả thực hiện</h2><br />
-				<h2><?php echo $_POST["title_wish"] ?></h2>
+				<h2><?php echo $_GET['title_wish'] ?></h2>
                 <div id="checkinresultsample">
                 <?php 
 					date_default_timezone_set('Asia/Ho_Chi_Minh');
