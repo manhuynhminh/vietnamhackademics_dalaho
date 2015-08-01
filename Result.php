44cd25ae029e13f67d9d2ddef4062c76c23210fb
2tran <manhhai_thk7@yahoo.com.vn>
2015-08-01 22:40:51 +0700

    page: Rate and result (php only)

---
+++ Result.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<link type="text/css" rel="stylesheet" href="/css/index.css" media="screen" />
+<link type="text/css" rel="stylesheet" href="/css/jquery.fullPage.css" media="screen" />
+
+<meta charset="utf-8">
+<meta name="description" content="Input Desire Learning">            
+
+<html lang="en">
+<head>
+	<title>Result</title>
+</head>
+<body id="mainsite">
+<?php 
+	if (isset($_GET['btsubmit_rate']))
+	{
+		echo "result today: ".$_GET['your_rate']."<br/>";
+		echo "You should be (call the advice from database)";
+	}
+	if (isset($_POST['btsubmit_report']))
+	{
+		echo "Your preview result is good, but you can try to better >_>";
+	}
+?>
+
+</body>
+
+</html>
