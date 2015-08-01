789b00fa6e1a81f90b6d42a9f0bc3b0757ce2e8a
Thuan Thanh <luongthuanthanh@yahoo.com>
2015-08-01 20:04:26 +0700

    chinh giao dien MainPage.php

--- MainPage.php
+++ MainPage.php
@@ -1,19 +1,27 @@
 ﻿<!DOCTYPE html>
 <link type="text/css" rel="stylesheet" href="/css/index.css" media="screen" />
 <link type="text/css" rel="stylesheet" href="/css/jquery.fullPage.css" media="screen" />
+<link rel="shortcut icon" href="/favicon.ico">
 
 <html lang="en">
-<body>
-                <h2>Theo dõi kết quả thực hiện</h2><br />
-				<h2><?php echo $_GET['title_wish'] ?></h2>
-                <div id="checkinresultsample">
-                <?php 
-					date_default_timezone_set('Asia/Ho_Chi_Minh');
-					$datesys = date('d/m/Y');
-					$dataapp = (int)($datesys - date('01/08/2015'));
-					echo $dataapp;
-				?><a href="hackathon.html">Days</a>
-                    <img />
-                </div>
+<head>
+	<title>Tracking Learning</title>
+</head>
+<body id="mainsite">
+     <div id="section3" class="section">
+        <h2>Learning Goal</h2>
+           <input type="text" name ="title_wish" class="textbox" readonly value="<?php echo $_GET['title_wish'] ?>"/>
+        <div id="times">
+          <a href="hackathon.html">
+			  <?php 
+                  date_default_timezone_set('Asia/Ho_Chi_Minh');
+                  $datesys = date('d/m/Y');
+                  $dataapp = (int)($datesys - date('01/08/2015'));
+                  echo $dataapp;
+              ?> days
+           </a>
+          <img />
+        </div>
+     </div>
 </body>
 </html>
