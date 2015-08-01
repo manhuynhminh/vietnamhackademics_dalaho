63f3cd577db8357ed9fc0b4bd085c76957981aa1
2tran <manhhai_thk7@yahoo.com.vn>
2015-08-01 17:22:02 +0700

    Main page <code php>

---
+++ MainPage.php
@@ -0,0 +1,19 @@
+﻿<html lang="en">
+<body>
+
+
+                <h2>Theo dõi kết quả thực hiện</h2><br />
+				<h2><?php echo $_POST["title_wish"] ?></h2>
+                <div id="checkinresultsample">
+                <?php 
+					date_default_timezone_set('Asia/Ho_Chi_Minh');
+					$datesys = date('d/m/Y');
+					echo $datesys;
+				?>
+                    <img />
+                </div>
+               
+            
+        
+</body>
+</html>
